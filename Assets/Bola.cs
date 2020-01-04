d5da47683fc2e3ee939cf8bf2eca9f576b7aac1a
DAVICORREIA21 <davicorreia21@gmail.com>
2020-01-04 12:00:07 -0300

    Adicionado Script

--- Assets/Bola.cs
+++ Assets/Bola.cs
@@ -5,9 +5,14 @@ using UnityEngine;
 public class Bola : MonoBehaviour
 {
     // Start is called before the first frame update
+    public float Speed = 5f;
+
     void Start()
     {
-        
+        float Speedx = Random.Range(0,2) == 0 ? -1 :1;
+        float Speedy = Random.Range(0,2) == 0 ? -1 :1;
+
+        GetComponent<Rigidbody>().velocity = new Vector3(Speed * Speedx , Speed * Speedy,0f); 
     }
 
     // Update is called once per frame
